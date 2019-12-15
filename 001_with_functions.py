af8eb6ee6d2f72741c1ddcd1246622768b8280f0
giadarol <giovanni.iadarola@cern.ch>
2019-12-15 18:38:43 +0100

    Generated pysixtrak model (problem with cavity frequancy)

--- 001_with_functions.py
+++ 001_with_functions.py
@@ -1,6 +1,11 @@
+import gc
+
 import bb_setup as bbs
 
+generate_pysixtrack_lines = True
+
 sequence_fname = 'mad/lhc_without_bb.seq'
+sequence_for_tracking_fname = 'mad/lhc_without_bb_fortracking.seq'
 ip_names = ['ip1', 'ip2', 'ip5', 'ip8']
 
 circumference = 26658.8832
@@ -48,6 +53,10 @@ bbs.get_geometry_and_optics_b1_b2(mad, bb_df_b1, bb_df_b2)
 # Get the position of the IPs in the surveys of the two beams
 ip_position_df = bbs.get_survey_ip_position_b1_b2(mad, ip_names)
 
+# Done with this madx model (we free some memory)
+del(mad)
+gc.collect()
+
 # Get geometry and optics at the partner encounter
 bbs.get_partner_corrected_position_and_optics(
         bb_df_b1, bb_df_b2, ip_position_df)
@@ -60,3 +69,18 @@ for bb_df in [bb_df_b1, bb_df_b2]:
 
 # Get bb dataframe for beam 4
 bb_df_b4 = bbs.get_counter_rotating(bb_df_b2)
+
+# Mad model of the machine to be tracked (bb is still dummy)
+mad_track_b1 = bbs.build_mad_instance_with_bb(
+    sequences_file_name=sequence_for_tracking_fname,
+    bb_data_frames=[bb_df_b1],
+    beam_names=['b1'],
+    sequence_names=['lhcb1'],
+    mad_echo=False, mad_warn=False, mad_info=False)
+
+if generate_pysixtrack_lines:
+    # Build pysixtrack b1 model
+    import pysixtrack
+    line_for_tracking_b1 = pysixtrack.Line.from_madx_sequence(
+        mad_track_b1.sequence["lhcb1"]
+    )
